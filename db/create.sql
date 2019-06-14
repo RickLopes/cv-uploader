b42092098a6894560bc240c4f7d6c1638f8a8fb7
RickLopes <32683600+RickLopes@users.noreply.github.com>
2019-06-14 17:03:43 +0100

    Update create.sql
    corrected data type of 'cv_file'

--- db/create.sql
+++ db/create.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS `candidates` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `full_name` varchar(255) NOT NULL,
   `birthdate` varchar(255) NOT NULL,
-  `cv_file` varchar(255) NOT NULL,
+  `cv_file` blob NOT NULL,
   PRIMARY KEY (`id`)
 );
